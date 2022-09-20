afba06b17406e318c1b0715f14c861f0e05dd11e
HNodeland <haakon@nodeland.no>
2022-09-20 11:56:40 +0200

    FIXED wrong username/password notification

--- task1/app/routes.py
+++ task1/app/routes.py
@@ -15,11 +15,11 @@ def index():
     if form.login.is_submitted() and form.login.submit.data:
         user = query_db('SELECT * FROM Users WHERE username="{}";'.format(form.login.username.data), one=True)
         if user == None:
-            flash('Sorry, this user does not exist!')
+            flash('Sorry, wrong password or username!')
         elif user['password'] == form.login.password.data:
             return redirect(url_for('stream', username=form.login.username.data))
         else:
-            flash('Sorry, wrong password!')
+            flash('Sorry, wrong password or username!')
 
     elif form.register.is_submitted() and form.register.submit.data:
         query_db('INSERT INTO Users (username, first_name, last_name, password) VALUES("{}", "{}", "{}", "{}");'.format(form.register.username.data, form.register.first_name.data,
