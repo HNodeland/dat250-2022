38a6e8a45a406f297048e68c9cf5d9ec8573b521
Haakon Webb <haakonwebb@gmail.com>
2022-09-23 16:03:45 +0200

    Changed it so that a file is no longer required to create a post

--- task1/app/forms.py
+++ task1/app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, FormField, TextAreaField, FileField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import InputRequired, EqualTo, Regexp, Length
-from flask_wtf.file import FileAllowed, FileRequired
+from flask_wtf.file import FileAllowed
 
 # defines all forms in the application, these will be instantiated by the template,
 # and the routes.py will read the values of the fields
@@ -29,7 +29,7 @@ class IndexForm(FlaskForm):
 
 class PostForm(FlaskForm):
     content = TextAreaField('New Post', render_kw={'placeholder': 'What are you thinking about?'})
-    image = FileField('Image', validators=[FileRequired(), FileAllowed(['jpg','png'], 'Images only!')])
+    image = FileField('Image', validators=[FileAllowed(['jpg','png'], 'Images only!')])
     submit = SubmitField('Post')
 
 class CommentsForm(FlaskForm):
